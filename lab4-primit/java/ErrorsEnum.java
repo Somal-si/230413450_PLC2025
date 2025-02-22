41fc113d33843144ac34e90d8bedc6782bd12165
Somal-si <230413450@aston.ac.uk>
2025-02-22 00:54:14 +0000

    lab4

--- lab4-primit/java/ErrorsEnum.java
+++ lab4-primit/java/ErrorsEnum.java
@@ -4,8 +4,10 @@ import java.util.EnumSet;
 public class ErrorsEnum
 {
     enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
+    //enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
 
     enum Result { A_BIT_DIFFERENT, INFINITY, ZERO, VERY_DIFFERENT }
+    //enum Result { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
     
     private static <E extends Enum<E>> E getEnumElement(String elementTypeName, Class<E> elementType)
     {
@@ -32,38 +34,38 @@ public class ErrorsEnum
         return result;
     }
   
-    private static Result error2Result(Error e)
+    private static Error result2Error(Result r)
     {
-        Result result = null;
+        Error error = null;
         
-        switch (e) {
-        case FP_ROUNDING:
-            result = Result.A_BIT_DIFFERENT;
+        switch (r) {
+        case A_BIT_DIFFERENT:
+            error = Error.FP_ROUNDING;
             break;
-        case FP_OVERFLOW:
-            result = Result.INFINITY;
+        case INFINITY:
+            error = Error.FP_OVERFLOW;
             break;
-        case FP_UNDERFLOW:
-            result = Result.ZERO;
+        case ZERO:
+            error = Error.FP_UNDERFLOW;
             break;
-        case INT_OVERFLOW:
-            result = Result.VERY_DIFFERENT;
+        case VERY_DIFFERENT:
+            error = Error.INT_OVERFLOW;
             break;
         }
         
-        return result;
+        return error;
     }
 
     public static void main(String[] args)
     {
         System.out.print("Known errors = ");
-        for (Error e : EnumSet.allOf(Error.class)) 
+        for (Result r : EnumSet.allOf(Result.class)) 
         {
-            System.out.print(e + " ");
+            System.out.print(r + " ");
         }
         System.out.println();
         
-        Error e = getEnumElement("error", Error.class);
-        System.out.println(e + " results in: " + error2Result(e));
+        Result r = getEnumElement("result", Result.class);
+        System.out.println(r + " error in: " + result2Error(r));
     }
 }
