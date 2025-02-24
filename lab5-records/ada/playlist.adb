f4c1e897a68423f25057cda34d711c3954740e0d
Somal-si <230413450@aston.ac.uk>
2025-02-24 00:58:01 +0000

    lab5 part1

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -28,6 +28,10 @@ procedure PlayList is
         Put (" (");
         Put (i.length_secs, aft => 1, exp => 0);
         Put ("s)");
+        Put (To_String (i.item_variant));
+        Put ("PAUSE");
+        Put (i.length_secs, aft => 1, exp => 0);
+        Put ("s)");
     end Put_Item;
 
     piece1 : Item :=
@@ -36,15 +40,15 @@ procedure PlayList is
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+    pause1 : Item :=
+       (
+         item_variant => PAUSE,
+         length_secs => 5.0
+       );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
